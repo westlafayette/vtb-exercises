a256f5807f505f91918400103b1fc3606a8760f8
max <maxim.khagay@btsdigital.kz>
2021-03-27 11:52:25 +0600

    feat(exercise_07): added code samples

---
+++ exercise_07/src/main/java/lectures/p2_race_condition/Counter.java
@@ -0,0 +1,19 @@
+package lectures.p2_race_condition;
+
+public class Counter {
+    private int c;
+
+    public int value() { return c; }
+
+    public Counter() {
+        c = 0;
+    }
+
+    public void inc() {
+        c++;
+    }
+
+    public void dec() {
+        c--;
+    }
+}
